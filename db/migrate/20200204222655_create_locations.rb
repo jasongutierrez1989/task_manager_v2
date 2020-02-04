3b0dae704b8c356333101299857df83a28ecb9e8
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-02-04 14:46:13 -0800

    set up locations table

--- db/migrate/20200204222655_create_locations.rb
+++ db/migrate/20200204222655_create_locations.rb
@@ -1,7 +1,11 @@
 class CreateLocations < ActiveRecord::Migration[6.0]
   def change
     create_table :locations do |t|
-
+      t.string :name
+      t.string :description
+      t.integer :task_id
+      t.integer :user_id
+      
       t.timestamps
     end
   end
