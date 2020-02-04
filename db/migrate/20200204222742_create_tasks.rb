84db07c76fb19f827b211701efcbf2f4a688599c
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-02-04 14:47:51 -0800

    set up tasks table

--- db/migrate/20200204222742_create_tasks.rb
+++ db/migrate/20200204222742_create_tasks.rb
@@ -1,6 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.integer :user_id
+      t.integer :location_id
 
       t.timestamps
     end
