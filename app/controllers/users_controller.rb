80d4e0bc8e2a2cdb8d18e5f8d4f41055cd440454
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-02-04 18:36:06 -0800

    set up basic show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       flash[:success] = "User was created successfully!"
-      redirect_to user_path
+      redirect_to user_path @user
     else
-      flash[:danger] = "Information not saved, try agin"
       render 'new'
+      flash[:danger] = "Information not saved, try again"
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
 private
