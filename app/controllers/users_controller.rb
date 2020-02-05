0e1b067941eeb09780c79ebc30fe93d8ef9d78a9
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-02-04 19:51:44 -0800

    linked user index to user show page. Created edit page and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,28 @@ class UsersController < ApplicationController
       flash[:success] = "User was created successfully!"
       redirect_to user_path @user
     else
-      render 'new'
       flash[:danger] = "Information not saved, try again"
+      render 'new'
     end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Updated Profile!"
+      redirect_to user_path
+    else
+      flash[:danger] = "Not saved, please try again"
+      render 'edit'
+    end
   end
 
 private
