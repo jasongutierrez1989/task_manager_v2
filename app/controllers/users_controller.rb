3fbc0755458a0d4d9b61a83f7ce74902d7b35651
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-02-04 17:38:13 -0800

    set up create action for users. added path info to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      flash[:success] = "User was created successfully!"
+      redirect_to user_path
+    else
+      flash[:danger] = "Information not saved, try agin"
+      render 'new'
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :job)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks
   has_many :locations, through: :tasks
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  has_secure_password
 end
