160ea5dff28a508bd1cfed1b94c18a95d779ffe8
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-02-05 16:25:43 -0800

    created tasks controller actions. New, create, edit, update, destroy

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,5 @@
 class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
 
   def index
     @locations = Location.all
@@ -20,15 +21,12 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @location = Location.find(params[:id])
   end
 
   def edit
-    @location = Location.find(params[:id])
   end
 
   def update
-    @location = Location.find(params[:id])
     if @location.update_attributes(location_params)
       flash[:success] = "Updated Location!"
       redirect_to location_path
@@ -38,9 +36,16 @@ class LocationsController < ApplicationController
     end
   end
 
+  def destroy
+    @location.destroy
+  end
 
 private
   def location_params
     params.require(:location).permit(:name, :description)
+  end
+
+  def set_location
+    @location = Location.find(params[:id])
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,51 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "New Task created!"
+      redirect_to task_path @task
+    else
+      flash[:danger] = "Task not created"
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update_attributes(task_params)
+      flash[:success] = "Updated task!"
+      redirect_to task_path
+    else
+      flash[:danger] = "Not saved, please try again"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+private
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
